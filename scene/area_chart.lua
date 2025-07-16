64e6c715d5c9dc3aea93655bec4d15993ca0bfdc
sea-script <arbanixsalah@gmail.com>
2025-07-16 19:05:28 +0100

    99% done, only some small tweaks left

--- scene/area_chart.lua
+++ scene/area_chart.lua
@@ -11,9 +11,17 @@ function area_chart.load()
     input_button_y.height = love.graphics.getFont():getHeight() + input_button_y.margin
 
     --the main thing of this file
-    areas = {}
-    areas[1] = { x = display.x, y = display.y + display.height }
-    areas[2] = { x = display.x + display.width, y = display.y + display.height }
+    areas1 = {}
+    areas1[1] = { x = display.x, y = display.y + display.height }
+    areas1[2] = { x = display.x + display.width, y = display.y + display.height }
+
+    areas2 = {}
+    areas2[1] = { x = display.x, y = display.y + display.height }
+    areas2[2] = { x = display.x + display.width, y = display.y + display.height }
+
+    areas3 = {}
+    areas3[1] = { x = display.x, y = display.y + display.height }
+    areas3[2] = { x = display.x + display.width, y = display.y + display.height }
 
     --to display the numbers on the axis
     y_values = {}
@@ -24,7 +32,7 @@ function area_chart.load()
     for i = 1, 9 do
         x_values[i] = 0
     end
-
+    --reset all
     y_num_input_mode = false
     x_num_input_mode = false
 
@@ -32,6 +40,18 @@ function area_chart.load()
     mouse_y_display = 0
 
     display.xaxis_input = 0
+
+    layer1 = { x = display.x + display.width, y = display.y, height = 40, selected = false }
+    layer1.margin = 5
+    layer1.width = width - layer1.x - 2 * layer1.margin
+    layer2 = { x = display.x + display.width, y = display.y + layer1.height + layer1.margin, height = 40, selected = false }
+    layer2.margin = 5
+    layer2.width = layer1.width
+    layer3 = { x = display.x + display.width, y = display.y + (layer2.height + layer2.margin) * 2, height = 40, selected = false }
+    layer3.margin = 5
+    layer3.width = layer1.width
+
+    layers = { layer1, layer2, layer3 }
 end
 
 function area_chart.update(dt)
@@ -47,12 +67,44 @@ function area_chart.update_x_axis()
 end
 
 function area_chart.mousepressed(mouse_x, mouse_y, mouseID)
-    if mouseID == 1  then
-        --drawing
-        if mouse_x >= areas[#areas-1].x and (mouse_x >= display.x and mouse_x <= display.x + display.width and mouse_y >= display.y and mouse_y <= display.y + display.height ) then
-                areas[#areas] = { x = mouse_x, y = mouse_y }
-                areas[#areas + 1] = { x = display.x + display.width, y = display.y + display.height }   
+    --HELP ME
+    if mouseID == 1 then
+        --drawing the area
+        if (mouse_x >= display.x and mouse_x <= display.x + display.width and mouse_y >= display.y and mouse_y <= display.y + display.height) then
+            if layer1.selected == true then
+                for i = 1, #areas1 - 1 do
+                    --if x is between 2 points.x
+                    if mouse_x > areas1[i].x and mouse_x < areas1[i + 1].x then
+                        table.insert(areas1, i + 1, { x = mouse_x, y = mouse_y }) --handles pushing all the elements  ""ILOVE INSERTTTTT!!""
+                        break
+                    elseif mouse_x == areas1[i].x and mouse_x == areas1[i].x then
+                        areas1[i].y = mouse_y
+                    end
+                end
+            elseif layer2.selected == true then
+                for i = 1, #areas2 - 1 do
+                    --if x is between 2 points.x
+                    if mouse_x > areas2[i].x and mouse_x < areas2[i + 1].x then
+                        table.insert(areas2, i + 1, { x = mouse_x, y = mouse_y }) --handles pushing all the elements  ""ILOVE INSERTTTTT!!""
+                        break
+                    elseif mouse_x == areas2[i].x and mouse_x == areas2[i].x then
+                        areas2[i].y = mouse_y
+                    end
+                end
+            elseif layer3.selected == true then
+                for i = 1, #areas3 - 1 do
+                    --if x is between 2 points.x
+                    if mouse_x > areas3[i].x and mouse_x < areas3[i + 1].x then
+                        table.insert(areas3, i + 1, { x = mouse_x, y = mouse_y }) --handles pushing all the elements  ""ILOVE INSERTTTTT!!""
+                        break
+                    elseif mouse_x == areas3[i].x and mouse_x == areas3[i].x then
+                        areas3[i].y = mouse_y
+                    end
+                end
+            end
         end
+
+
         --first button
         if mouse_x >= input_button_x.x and mouse_x <= input_button_x.x + input_button_x.width and mouse_y >= input_button_x.y and mouse_y <= input_button_x.y + input_button_x.height then
             --enable typing mode, it will show a text saying ti enter a number
@@ -66,6 +118,17 @@ function area_chart.mousepressed(mouse_x, mouse_y, mouseID)
             x_num_input_mode = false
             user_input = ""
         end
+
+        --buttons for layers
+        for i = 1, #layers do
+            if (mouse_x >= layers[i].x and mouse_x <= layers[i].x+layers[i].width and mouse_y >= layers[i].y and mouse_y <= layers[i].y+layers[i].height) then
+                for j = 1, #layers do
+                    layers[j].selected = false
+                end
+                layers[i].selected = true
+            end
+        end
+            
     end
 end
 
@@ -73,14 +136,15 @@ function area_chart.keypressed(key)
     --end typing mode
     if key == "return" then
         if x_num_input_mode and user_input ~= "" then
-            x_num_input_mode = false                                                                              --de-activate input mode
-            local num = tonumber(user_input)                                                                      --convert to number
-            display.xaxis_input = num                                                                             --store the number on the display var
-            input_button_x.value = tostring(num)                                                                  --to print the value
+            x_num_input_mode = false             --de-activate input mode
+            local num = tonumber(user_input)     --convert to number
+            display.xaxis_input =
+            num                                  --store the number on the display var
+            input_button_x.value = tostring(num) --to print the value
             user_input =
-            ""                                                                                                    --empty it
+            ""                                   --empty it
             input_button_x.width = love.graphics.getFont():getWidth(input_button_x.value) +
-            input_button_x.margin                                                                                 --update the width
+                input_button_x.margin            --update the width
             --update all
             area_chart.update_x_axis()
         elseif y_num_input_mode and user_input ~= "" then
@@ -98,6 +162,9 @@ function area_chart.keypressed(key)
             end
         end
         --restart all points
+        areas1 = {}
+        areas1[1] = { x = display.x, y = display.y + display.height }
+        areas1[2] = { x = display.x + display.width, y = display.y + display.height }
     end
 end
 
@@ -114,7 +181,7 @@ function area_chart.draw()
     love.graphics.setColor(white)
     love.graphics.print("area_chart")
 
-    
+
     --Y values
     love.graphics.setColor(white)
     love.graphics.rectangle("line", input_button_y.x, input_button_y.y, input_button_y.width, input_button_y.height)
@@ -136,18 +203,46 @@ function area_chart.draw()
 
     --Y values
     for i = 1, 9 do
-        love.graphics.print(x_values[i], (y_step_size * (i+1)) - 20 , display.y+display.height + 5)
+        love.graphics.print(x_values[i], (y_step_size * (i + 1)) - 20, display.y + display.height + 5)
     end
 
-    if #areas >= 3 then
+    --HELP ME
+    if #areas1 >= 3 then
         --must convert to a normal table, with just number
         local points = {}
-        for i = 1, #areas do
-            table.insert(points, areas[i].x)
-            table.insert(points, areas[i].y)
+        for i = 1, #areas1 do
+            table.insert(points, areas1[i].x)
+            table.insert(points, areas1[i].y)
         end
+        love.graphics.setColor(white)
         love.graphics.polygon("line", points)
+    end
+    if #areas2 >= 3 then
+        --must convert to a normal table, with just number
+        local points = {}
+        for i = 1, #areas2 do
+            table.insert(points, areas2[i].x)
+            table.insert(points, areas2[i].y)
+        end
+        love.graphics.setColor(green)
+        love.graphics.polygon("line", points)
+    end
+    if #areas3 >= 3 then
+        --must convert to a normal table, with just number
+        local points = {}
+        for i = 1, #areas3 do
+            table.insert(points, areas3[i].x)
+            table.insert(points, areas3[i].y)
+        end
+        love.graphics.setColor(blue)
+        love.graphics.polygon("line", points)
+    end
+    for i = 1, #layers do
+        love.graphics.setColor(1 / i, 1 / i, (1 / i) * 2)
+        love.graphics.rectangle("fill", layers[i].x, layers[i].y, layers[i].width, layers[i].height)
     end
 end
 
 return area_chart
+
+--layers
